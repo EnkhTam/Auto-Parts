8ef3d10c8561a21e965ab7ba30f2f01c4deb11aa
WeizhengJ <70171753+WeizhengJ@users.noreply.github.com>
2021-04-21 12:19:13 -0500

    Update drawtable.php

--- drawtable.php
+++ drawtable.php
@@ -1,12 +1,11 @@
 <?php
 
 
-function draw_table($rows, $name) {
+function draw_table($rows) {
 	echo "<table border=1, cellspacing=1>";
 	echo "<tr>";
 	foreach($rows[0] as $key => $item) {
 		echo "<th>$key</th>";
-		echo "<th>Description</th>";
 		
 
 	}
@@ -15,7 +14,7 @@ function draw_table($rows, $name) {
 		echo "<tr>";
 		foreach($row as $key => $item) {
 			echo"<td>$item</td>";
-			echo "<td>$name</td>";
+
 		
 }
 	echo"</tr>";
